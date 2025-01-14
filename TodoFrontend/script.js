c5aaa859bc0f569b605c6886d704091d69c5e240
Jenny Evertsson <jentap@gmail.com>
2025-01-14 11:00:11 +0100

    Printing backend data prettily.

--- TodoFrontend/script.js
+++ TodoFrontend/script.js
@@ -1,3 +1,4 @@
+
 function callTheBackend() {
     fetch('https://localhost:7217/Project')
         .then(response => response.json())
@@ -22,4 +23,53 @@ function callTheBackend() {
         });
 }
 
-callTheBackend();
+function showProjects(){
+    fetch('https://localhost:7217/Project')
+        .then(response => response.json())
+        .then(function (data){
+            document.getElementById("nowShowing").innerHTML = "Now showing Projects";
+            clearData();
+            let target = document.getElementById("projects");
+            for (const project of data){
+                helperMethod("h1", project.name, target);
+            }
+        });
+}
+function showTasks(){
+    fetch('https://localhost:7217/Task')
+        .then(response => response.json())
+        .then(function (data){
+            clearData();
+            document.getElementById("nowShowing").innerHTML = "Now showing Tasks";
+            let target = document.getElementById("tasks");
+            for (const task of data){
+                helperMethod("h2", task.description, target);
+            }
+        });
+}
+function showTags(){
+    fetch('https://localhost:7217/Tag')
+        .then(response => response.json())
+        .then(function (data){
+            document.getElementById("nowShowing").innerHTML = "Now showing Tags";
+            clearData();
+            let target = document.getElementById("tags");    
+            for (const tag  of data){
+                helperMethod("h3", tag.name, target);
+            }
+        });
+}
+
+
+function helperMethod(elementType, dataValue, target) {
+    let newElement= document.createElement(elementType);
+    //let dbData = document.createTextNode(dataValue);
+    //newElement.appendChild(dbData);
+    newElement.innerHTML = dataValue;
+    target.appendChild(newElement);
+}
+function clearData(){
+    document.getElementById("projects").innerHTML = "";
+    document.getElementById("tags").innerHTML = "";
+    document.getElementById("tasks").innerHTML = "";
+}
