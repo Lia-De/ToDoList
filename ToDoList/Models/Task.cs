0aca29736deb5a2bb35a964a71b4d71179012e20
Jenny Evertsson <jentap@gmail.com>
2025-01-22 13:07:52 +0100

    Changing name on Task.Description to Task.Name

--- ToDoList/Models/Task.cs
+++ ToDoList/Models/Task.cs
@@ -3,7 +3,7 @@
 public class Task
 {
     public int TaskId { get; set; }
-    public required string Description { get; set; }
+    public required string Name { get; set; }
     public List<Tag> Tags { get; set; } = new List<Tag>();
     public DateTime? Deadline { get; set; }
     public ToDoStatus? Status { get; set; }
