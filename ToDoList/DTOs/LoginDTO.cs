7329636a603dec3601d28d62d736442890db9555
Jenny Evertsson <jentap@gmail.com>
2025-03-29 18:02:11 +0100

    Refactor, to connect the API (on azure) to the SQL server (also on azure)

--- ToDoList/DTOs/LoginDTO.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ToDoList.DTOs;
-
-public class LoginDTO
-{
-    public string? FirstName { get; set; }
-    public string? LastName { get; set; }
-    public string? Email { get; set; }
-    public string? Password { get; set; }
-}
