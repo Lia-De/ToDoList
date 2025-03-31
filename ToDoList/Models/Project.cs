ef57857f2a1ac3383bf70ef189db73dd86228d4d
Jenny Evertsson <jentap@gmail.com>
2025-03-31 18:52:05 +0200

    simplified initializers

--- ToDoList/Models/Project.cs
+++ ToDoList/Models/Project.cs
@@ -7,14 +7,14 @@ public class Project
 {
     [Key] public int ProjectId { get; set; }
     [MaxLength(50)] public required string Name { get; set; }
-    public List<Task> Tasks { get; set; } = new List<Task>();
-    public List<Tag> Tags { get; set; } = new List<Tag>();
+    public List<Task> Tasks { get; set; } = [];
+    public List<Tag> Tags { get; set; } = [];
     public ToDoStatus Status { get; set; }
     public DateTime ProjectCreated { get; private set; } = DateTime.Now;
     //public TimeSpan TotalWorkingTime { get; set; } = TimeSpan.Zero;
     public bool HasTimerRunning { get; set; } = false;
     public string Description { get; set; } = string.Empty;
-    public List<ProjectTimer> Timers { get; set; } = new List<ProjectTimer> { };
+    public List<ProjectTimer> Timers { get; set; } = [];
 
     [Column("TotalWorkingTime")]
     public long TotalWorkingTimeSeconds
